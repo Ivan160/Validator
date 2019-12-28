f37bde2053419667bb632f30b3d03fda27787e19
Ivan160 <dfyzdfyz16@gmail.com>
2019-12-29 02:26:17 +1000

    Validator.js 12/29/2019

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+﻿const path = require('path');
+
+module.exports = {
+   entry: {                                                                  
+      main: './src/validator.js'
+   },
+   output: {                                                                
+      path: path.resolve(__dirname, './dist'),
+      filename: '[name].js',
+      publicPath: '/dist'
+   },
+   devServer: {                                                          
+      overlay: true
+   },
+   module: {                                                         
+      rules: [                                                         
+         {                                                               
+            test: /\.js$/,                                          
+            exclude: '/node_modules/'                          
+         }
+      ]
+   }
+};
