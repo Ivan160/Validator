27e9a00f5d8aa9b12f89bb1803ada3a5772a08e8
Ivan160 <dfyzdfyz16@gmail.com>
2019-12-30 19:14:34 +1000

    Validator.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import validator from './validator';
-
-addEventListener('DOMContentLoaded', function() {
-   var forms = document.querySelectorAll('form');
-   for (var i = 0; i < forms.length; i++) {
-      validator(forms[i]);
-   }
-});
